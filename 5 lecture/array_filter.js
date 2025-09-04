7da2a13f4bc3dc9202ee07174a6f426525c85870
priyanshu ranjan <priyanshu82ranjan48@gmail.com>
2025-09-04 09:18:52 +0530

    Delete array_filter.js

--- 5 lecture/array_filter.js
+++
@@ -1,7 +0,0 @@
-//creates new array of elements that give true for a conditon 
-arr=[2,4,4,75,68,4,35,]
-let newarr=arr.filter(function(value,index,array){
-    return(value%2==0)
-})
-
-console.log(newarr)
